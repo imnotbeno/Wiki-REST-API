01b50ea76b8d6210b13b27c348d2d0411e52f74b
Niko Bec <nikobec96@gmail.com>
2020-11-07 17:28:34 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+
+
+
+app.listen(3000,function(){
+    console.log("Server set on port 3000!");
+});
