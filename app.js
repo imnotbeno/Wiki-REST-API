dcbdefe211de33de36edccb84c39e4352ed1feb5
Niko Bec <nikobec96@gmail.com>
2020-11-07 17:39:42 +0100

    some additional boiler code

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose");
 
+//Connection to mongo database
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser : true, useUnifiedTopology : true});
 
+//Initialise ejs module
+app.set("view engine", "ejs");
+
+//Initialise body-parser module
+app.use(bodyParser.urlencoded({extended:true}));
+
+//app.use(express.static("public"));
 
 app.listen(3000,function(){
     console.log("Server set on port 3000!");
